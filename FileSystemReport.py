bffd43b2c4e8e26a02b18e1afd6d9f1be7d85ff4
Brenden Smith <bcsmith2846@gmail.com>
2020-07-01 10:46:16 -0500

    Change min folder size to 500 MB

--- FileSystemReport.py
+++ FileSystemReport.py
@@ -6,7 +6,7 @@ def main():
     print("Dir: " + start)
     for dir in [os.path.join(start,dI) for dI in os.listdir(start) if os.path.isdir(os.path.join(start,dI))]:
         size = getSizes(dir)
-        if size > 100.0:
+        if size > 500.0:
             print(f"{dir:<150}  {size:>20.2f}MB")
             
         
